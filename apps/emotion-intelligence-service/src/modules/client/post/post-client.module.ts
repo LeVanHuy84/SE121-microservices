cbd36f97c031ec829ae046fbe73fa604bc37f371
Le Van Huy <huyle842005@gmail.com>
2026-08-10 17:04:18 +0700

    chore(user-social): remove typeorm dependencies and update tsconfig

---
+++ apps/emotion-intelligence-service/src/modules/client/post/post-client.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    ClientsModule.registerAsync([
+      {
+        name: 'POST_SERVICE',
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (config: ConfigService) => ({
+          transport: Transport.TCP,
+          options: {
+            port: config.get<number>('CONTENT_FEED_SERVICE_PORT') || 4002,
+          },
+        }),
+      },
+    ]),
+  ],
+  exports: [ClientsModule],
+})
+export class PostClientModule {}
