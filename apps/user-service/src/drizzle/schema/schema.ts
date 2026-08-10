b563c9c8ff5fd7a95c72c5d5efe5c82f8b36b97a
Huy Le <huyle842005@gmail.com>
2026-08-10 17:12:54 +0700

    Revert "feat(user-social): migrate group module to drizzle orm"
    This reverts commit 90322b52903c6444928698d301f453e6739e0c6d.

--- apps/user-service/src/drizzle/schema/schema.ts
+++ apps/user-service/src/drizzle/schema/schema.ts
@@ -2,6 +2,3 @@ export * from './authorize.schema';
 export * from './users.schema';
 export * from './profiles.schema';
 export * from './outbox.schema';
-export * from './group.schema';
-export * from './social.schema';
-export { processedEvents } from './outbox.schema';
