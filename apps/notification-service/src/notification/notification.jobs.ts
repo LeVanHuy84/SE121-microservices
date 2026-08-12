a34f35f388a4fffae4ab368c57541de6a82c0e91
Le Van Huy <huyle842005@gmail.com>
2026-08-12 22:11:45 +0700

    chore: remove post-service and notification-service

--- apps/notification-service/src/notification/notification.jobs.ts
+++
@@ -1,8 +0,0 @@
-export const NOTIFICATION_QUEUE = 'notifications';
-
-
-export const REGULAR_NOTIFICATION_DELIVERY_JOB =
-  'deliver-regular-notification';
-export const CHAT_PUSH_DELIVERY_JOB = 'deliver-chat-push';
-export const CALL_PUSH_DELIVERY_JOB = 'deliver-call-push';
-export const CALL_CANCEL_PUSH_DELIVERY_JOB = 'deliver-call-cancel-push';
--- apps/notification-service/src/rabbitmq.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SharedModule } from './share.module';
-
-@Module({
-  imports: [SharedModule],
-})
-export class RabbitmqModule {}
