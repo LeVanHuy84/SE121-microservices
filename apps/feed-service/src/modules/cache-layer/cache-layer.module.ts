96353fe3d19aeb3665cdf82af600211bac014445
Le Van Huy <huyle842005@gmail.com>
2026-08-12 22:12:14 +0700

    chore: remove feed-service, logging-service, and media-service

--- apps/feed-service/src/modules/cache-layer/cache-layer.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { CacheLayerService } from './cache-layer.service';
-
-@Global()
-@Module({
-  providers: [CacheLayerService],
-  exports: [CacheLayerService],
-})
-export class CacheLayerModule {}
--- apps/media-service/src/cloudinary/cloudinary.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CloudinaryService } from './cloudinary.service';
-import { CloudinaryProvider } from './providers/cloudinary';
-
-@Module({
-  providers: [CloudinaryService, CloudinaryProvider],
-  exports: [CloudinaryProvider, CloudinaryService]
-})
-export class CloudinaryModule {}
