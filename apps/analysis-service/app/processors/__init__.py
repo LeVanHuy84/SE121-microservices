2f48c74e540852c7c6af91f2163a3476cd1f4608
Le Van Huy <huyle842005@gmail.com>
2026-08-14 20:44:52 +0700

    refactor: delete deprecated analysis-service and chatbot-service

--- apps/analysis-service/app/processors/__init__.py
+++
@@ -1,9 +0,0 @@
-# app/processors/__init__.py
-
-from .batch_processor import OutboxBatchProcessor
-from .retry_worker import RetryWorker
-
-__all__ = [
-    'OutboxBatchProcessor',
-    'RetryWorker',
-]
